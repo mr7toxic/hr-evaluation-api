af22526f11694cfb6244ff2d6c60fecea0d3f9f3
Amir <amir1998mousavi@gmail.com>
2025-11-29 14:12:30 +0330

    dockerized & Config DB in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>("DB_HOST"),
+        port: configService.getOrThrow<number>("DB_PORT") || 5432,
+        username: configService.getOrThrow<string>("DB_USER"),
+        password: configService.getOrThrow<string>("DB_PASS"),
+        database: configService.getOrThrow<string>("DB_NAME"),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
